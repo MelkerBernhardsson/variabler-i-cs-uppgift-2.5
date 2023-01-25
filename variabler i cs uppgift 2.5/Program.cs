eb52945df3e90da2ad8a7ef0108ab897922132a4
Pingvu <nhlpool27@gmail.com>
2023-01-25 10:35:55 +0100

    Add project files.

---
+++ variabler i cs uppgift 2.5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace uppgift5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vilket år är det");
+            int årtal = int.Parse(Console.ReadLine());
+            int tvåk = 2100 - årtal;
+            if (tvåk > 0)
+            {
+                Console.WriteLine("det är " + tvåk + " år kvar till 2100");
+            }
+            else
+            {
+                Console.WriteLine("2100 har redan passerat");
+            }
+        }
+    }
+}
